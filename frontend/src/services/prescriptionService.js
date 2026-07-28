3bf4481f88ad1a6ba5897e79eee23cb687acbf31
Madhi2728 <jmathi2728@gmail.com>
2026-07-28 20:20:25 +0530

    Investigated prescription upload failure

--- frontend/src/services/prescriptionService.js
+++ frontend/src/services/prescriptionService.js
@@ -3,9 +3,7 @@ import api from "./api";
 export async function uploadPrescription(file) {
   const formData = new FormData();
   formData.append("image", file);
-  const { data } = await api.post("/prescriptions", formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
+  const { data } = await api.post("/prescriptions", formData);
   return data.prescription;
 }
 
